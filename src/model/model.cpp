4455e04c9dcffa6f3f221c7d69feb16b6134af3a
Raphael Russo <122693743+rrussoneu@users.noreply.github.com>
2025-01-04 18:46:49 -0500

    add model class files

---
+++ src/model/model.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Raphael Russo on 1/4/25.
+//
+
+#include "model.h"
---
+++ src/model/model.h
@@ -0,0 +1,14 @@
+//
+// Created by Raphael Russo on 1/4/25.
+//
+
+#ifndef GARDEN_SIMULATION_MODEL_H
+#define GARDEN_SIMULATION_MODEL_H
+
+
+class Model {
+
+};
+
+
+#endif //GARDEN_SIMULATION_MODEL_H
