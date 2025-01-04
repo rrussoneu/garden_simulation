51989a5437c761170b3bdf8887a97a883b6a594f
Raphael Russo <122693743+rrussoneu@users.noreply.github.com>
2025-01-03 23:52:30 -0500

    implement getters/setters

--- src/renderer/camera.cpp
+++ src/renderer/camera.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "camera.h"
+
+
+void Camera::setAspectRatio(float ratio) {
+    m_aspectRatio = ratio;
+}
+
+void Camera::setPosition(const QVector3D &position) {
+    m_position = position;
+}
+
+void Camera::setTarget(const QVector3D &target) {
+    m_target = target;
+}
+
+QVector3D Camera::getPosition() {
+    return m_position;
+}
+
+QVector3D Camera::getTarget() {
+    return m_target;
+}
+
+float Camera::getDistance() {
+    return m_distance;
+}
+
+
+
