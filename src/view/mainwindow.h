6b3a6a6b6fe773757276098cfadca2e826b5d2b5
Raphael Russo <122693743+rrussoneu@users.noreply.github.com>
2025-01-11 17:59:09 -0500

    update UI with controller and moisture conditions

--- src/view/mainwindow.h
+++ src/view/mainwindow.h
@@ -10,7 +10,10 @@
 #include <QLabel>
 #include <QSlider>
 #include <QBoxLayout>
+#include <QCheckBox>
 #include "gardenglwidget.h"
+#include "controller/gardencontroller.h"
+#include "model/gardenmodel.h"
 
 
 class MainWindow : public QMainWindow {
@@ -25,20 +28,32 @@ signals:
 
 private slots:
     void handleTemperatureChange(int value);
+    void handleMoistureChange(int value);
+    void handleTemperatureSensorToggle(bool enabled);
+    void handleMoistureSensorToggle(bool enabled);
+    //void handleSaveGarden();
+    //void handleLoadGarden();
 
 
 private:
-
-    QDockWidget *m_toolsDock;
-    QDockWidget *m_tempDock;
-    QLabel *m_tempLabel;
-    QSlider *m_tempSlider;
-    GardenGLWidget* m_gardenWidget;
 
     void createMenus();
     void createDockWindows();
     void createToolbar();
     void setupConnections();
+
+    QDockWidget *m_toolsDock;
+    QDockWidget *m_environmentDock;
+    QLabel *m_tempLabel;
+    QSlider *m_tempSlider;
+    QLabel *m_moistureLabel;
+    QSlider *m_moistureSlider;
+    GardenGLWidget* m_gardenWidget;
+    QCheckBox *m_moistureSensorCheck;
+    QCheckBox *m_tempSensorCheck;
+
+    std::unique_ptr<GardenController> m_controller;
+    std::unique_ptr<GardenModel> m_model;
 
 
 
